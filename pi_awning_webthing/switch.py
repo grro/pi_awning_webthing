c57f9e5f529c727457b8ccc177d4620bc24d1770
grro <gregor.roth@1und1.de>
2024-05-26 15:01:52 +0200

    switch fixed

--- pi_awning_webthing/switch.py
+++ pi_awning_webthing/switch.py
@@ -31,6 +31,12 @@ class Switch:
     def is_backward(self) -> bool:
         return self.state[1]
 
+    def is_target_reached(self) -> bool:
+        for anwing in self.awnings:
+            if not anwing.is_target_reached():
+                return False
+        return True
+
     def on_switch_updated(self, pin: int):
         is_forward = GPIO.input(self.pin_forward) >= 1
         is_backward = GPIO.input(self.pin_backward) >= 1
@@ -38,27 +44,22 @@ class Switch:
         new_state = (is_forward, is_backward)
         try:
             if new_state == self.MOVE_FORWARD:
-                # repeated
-                if self.state == self.MOVE_FORWARD:
-                    for anwing in self.awnings:
-                        current_pos = anwing.get_position()
-                        anwing.set_position(current_pos)
-                # fresh
-                else:
-                    self.last_time_direction_changed = datetime.now()
+                if self.is_target_reached():
                     for anwing in self.awnings:
                         anwing.set_position(100)
-            elif new_state == self.MOVE_BACKWARD:
-                # repeated
-                if self.state == self.MOVE_BACKWARD:
+                else:
                     for anwing in self.awnings:
                         current_pos = anwing.get_position()
                         anwing.set_position(current_pos)
-                # fresh
-                else:
+
+            elif new_state == self.MOVE_BACKWARD:
+                if self.is_target_reached():
                     for anwing in self.awnings:
                         anwing.set_position(0)
-
+                else:
+                    for anwing in self.awnings:
+                        current_pos = anwing.get_position()
+                        anwing.set_position(current_pos)
         except Exception as e:
             logging.error(e)
         finally:
